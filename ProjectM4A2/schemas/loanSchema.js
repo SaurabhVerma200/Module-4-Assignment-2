24b3c322b65301804d80c0488801cd55ee99911e
SaurabhVerma200 <102269656+SaurabhVerma200@users.noreply.github.com>
2022-10-27 23:34:20 -0700

    Add files via upload

---
+++ ProjectM4A2/schemas/loanSchema.js
@@ -0,0 +1,26 @@
+/*
+Name: Nikita Goel
+ID: 1223438619
+Created Date: 10/3/2022
+Description: This file is used to create schema for validating the input of documents taken from the client which should be 
+in sync with the data types and fields mentioned in this schema. 
+*/
+
+
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const loanSchema = new Schema({
+  customerName:  String, 
+  phoneNumber: Number,
+  address:   String,
+  loanAmount: Number,
+  interest: Number,
+  loanTermYears: Number,
+  loanType: String,
+  description: String,
+  createdDate: { type: Date, default: Date.now },
+  insertedDate: { type: Date, default: Date.now },
+});
+
+const loans = mongoose.model('Loan', loanSchema);
